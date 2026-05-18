cf38c93d69980809e3511cd228fb8647a343b9a4
Jesterace <jesterace@jesterace.com>
2026-05-18 05:37:00 -0300

    Add JesterStep settings window

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,35 @@
 #include <windows.h>
 #include <shellapi.h>
 #include <string>
+#include <cstdlib>
 
 static const wchar_t* APP_CLASS = L"JesterStepPanel";
+static const wchar_t* SETTINGS_CLASS = L"JesterStepSettings";
+
 static const UINT APPBAR_CALLBACK = WM_APP + 100;
-static const int PANEL_HEIGHT = 36;
+
+static const int IDC_PANEL_POSITION = 2001;
+static const int IDC_PANEL_HEIGHT = 2002;
+static const int IDC_APPLY = 2003;
+static const int IDC_SAVE = 2004;
+static const int IDC_CANCEL = 2005;
+
+static HWND g_panel_hwnd = nullptr;
+static HWND g_settings_hwnd = nullptr;
 
 static bool g_appbar_registered = false;
+static bool g_panel_top = true;
+static int g_panel_height = 36;
+
+static int clamp_int(int value, int min_value, int max_value) {
+    if (value < min_value) {
+        return min_value;
+    }
+    if (value > max_value) {
+        return max_value;
+    }
+    return value;
+}
 
 static std::wstring get_time_text() {
     SYSTEMTIME st{};
@@ -25,16 +48,29 @@ static void position_appbar(HWND hwnd) {
     APPBARDATA abd{};
     abd.cbSize = sizeof(APPBARDATA);
     abd.hWnd = hwnd;
-    abd.uEdge = ABE_TOP;
+    abd.uEdge = g_panel_top ? ABE_TOP : ABE_BOTTOM;
+
+    int screen_w = GetSystemMetrics(SM_CXSCREEN);
+    int screen_h = GetSystemMetrics(SM_CYSCREEN);
 
     abd.rc.left = 0;
-    abd.rc.top = 0;
-    abd.rc.right = GetSystemMetrics(SM_CXSCREEN);
-    abd.rc.bottom = PANEL_HEIGHT;
+    abd.rc.right = screen_w;
+
+    if (g_panel_top) {
+        abd.rc.top = 0;
+        abd.rc.bottom = g_panel_height;
+    } else {
+        abd.rc.top = screen_h - g_panel_height;
+        abd.rc.bottom = screen_h;
+    }
 
     SHAppBarMessage(ABM_QUERYPOS, &abd);
 
-    abd.rc.bottom = abd.rc.top + PANEL_HEIGHT;
+    if (g_panel_top) {
+        abd.rc.bottom = abd.rc.top + g_panel_height;
+    } else {
+        abd.rc.top = abd.rc.bottom - g_panel_height;
+    }
 
     SHAppBarMessage(ABM_SETPOS, &abd);
 
@@ -93,12 +129,244 @@ static void notify_appbar_windowpos(HWND hwnd) {
     SHAppBarMessage(ABM_WINDOWPOSCHANGED, &abd);
 }
 
+static void apply_settings_from_window(HWND hwnd) {
+    HWND position_combo = GetDlgItem(hwnd, IDC_PANEL_POSITION);
+    HWND height_edit = GetDlgItem(hwnd, IDC_PANEL_HEIGHT);
+
+    LRESULT selected = SendMessageW(position_combo, CB_GETCURSEL, 0, 0);
+    g_panel_top = selected != 1;
+
+    wchar_t height_text[32]{};
+    GetWindowTextW(height_edit, height_text, 32);
+
+    int new_height = _wtoi(height_text);
+    g_panel_height = clamp_int(new_height, 24, 96);
+
+    wchar_t fixed_height[32]{};
+    swprintf_s(fixed_height, L"%d", g_panel_height);
+    SetWindowTextW(height_edit, fixed_height);
+
+    if (g_panel_hwnd) {
+        position_appbar(g_panel_hwnd);
+        InvalidateRect(g_panel_hwnd, nullptr, TRUE);
+    }
+}
+
+static LRESULT CALLBACK settings_wndproc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+    switch (msg) {
+        case WM_CREATE: {
+            CreateWindowExW(
+                0,
+                L"STATIC",
+                L"Panel position:",
+                WS_CHILD | WS_VISIBLE,
+                16,
+                24,
+                110,
+                24,
+                hwnd,
+                nullptr,
+                GetModuleHandleW(nullptr),
+                nullptr
+            );
+
+            HWND position_combo = CreateWindowExW(
+                0,
+                L"COMBOBOX",
+                nullptr,
+                WS_CHILD | WS_VISIBLE | CBS_DROPDOWNLIST,
+                140,
+                20,
+                170,
+                120,
+                hwnd,
+                (HMENU)IDC_PANEL_POSITION,
+                GetModuleHandleW(nullptr),
+                nullptr
+            );
+
+            SendMessageW(position_combo, CB_ADDSTRING, 0, (LPARAM)L"Top");
+            SendMessageW(position_combo, CB_ADDSTRING, 0, (LPARAM)L"Bottom");
+            SendMessageW(position_combo, CB_SETCURSEL, g_panel_top ? 0 : 1, 0);
+
+            CreateWindowExW(
+                0,
+                L"STATIC",
+                L"Panel height:",
+                WS_CHILD | WS_VISIBLE,
+                16,
+                64,
+                110,
+                24,
+                hwnd,
+                nullptr,
+                GetModuleHandleW(nullptr),
+                nullptr
+            );
+
+            wchar_t height_text[32]{};
+            swprintf_s(height_text, L"%d", g_panel_height);
+
+            CreateWindowExW(
+                WS_EX_CLIENTEDGE,
+                L"EDIT",
+                height_text,
+                WS_CHILD | WS_VISIBLE | ES_NUMBER,
+                140,
+                60,
+                80,
+                24,
+                hwnd,
+                (HMENU)IDC_PANEL_HEIGHT,
+                GetModuleHandleW(nullptr),
+                nullptr
+            );
+
+            CreateWindowExW(
+                0,
+                L"STATIC",
+                L"Height range: 24 to 96 px",
+                WS_CHILD | WS_VISIBLE,
+                140,
+                88,
+                180,
+                20,
+                hwnd,
+                nullptr,
+                GetModuleHandleW(nullptr),
+                nullptr
+            );
+
+            CreateWindowExW(
+                0,
+                L"BUTTON",
+                L"Apply",
+                WS_CHILD | WS_VISIBLE | BS_DEFPUSHBUTTON,
+                44,
+                132,
+                80,
+                28,
+                hwnd,
+                (HMENU)IDC_APPLY,
+                GetModuleHandleW(nullptr),
+                nullptr
+            );
+
+            CreateWindowExW(
+                0,
+                L"BUTTON",
+                L"Save",
+                WS_CHILD | WS_VISIBLE,
+                136,
+                132,
+                80,
+                28,
+                hwnd,
+                (HMENU)IDC_SAVE,
+                GetModuleHandleW(nullptr),
+                nullptr
+            );
+
+            CreateWindowExW(
+                0,
+                L"BUTTON",
+                L"Cancel",
+                WS_CHILD | WS_VISIBLE,
+                228,
+                132,
+                80,
+                28,
+                hwnd,
+                (HMENU)IDC_CANCEL,
+                GetModuleHandleW(nullptr),
+                nullptr
+            );
+
+            return 0;
+        }
+
+        case WM_COMMAND: {
+            int id = LOWORD(wparam);
+
+            if (id == IDC_APPLY) {
+                apply_settings_from_window(hwnd);
+                return 0;
+            }
+
+            if (id == IDC_SAVE) {
+                apply_settings_from_window(hwnd);
+                MessageBoxW(
+                    hwnd,
+                    L"Settings applied.\n\nSaving to settings.ini comes next in v0.4.",
+                    L"JesterStep",
+                    MB_OK | MB_ICONINFORMATION
+                );
+                return 0;
+            }
+
+            if (id == IDC_CANCEL) {
+                DestroyWindow(hwnd);
+                return 0;
+            }
+
+            return 0;
+        }
+
+        case WM_CLOSE:
+            DestroyWindow(hwnd);
+            return 0;
+
+        case WM_NCDESTROY:
+            g_settings_hwnd = nullptr;
+            return 0;
+    }
+
+    return DefWindowProcW(hwnd, msg, wparam, lparam);
+}
+
+static void show_settings_window(HWND parent) {
+    if (g_settings_hwnd) {
+        ShowWindow(g_settings_hwnd, SW_SHOW);
+        SetForegroundWindow(g_settings_hwnd);
+        return;
+    }
+
+    WNDCLASSW wc{};
+    wc.lpfnWndProc = settings_wndproc;
+    wc.hInstance = GetModuleHandleW(nullptr);
+    wc.lpszClassName = SETTINGS_CLASS;
+    wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+
+    RegisterClassW(&wc);
+
+    g_settings_hwnd = CreateWindowExW(
+        WS_EX_DLGMODALFRAME,
+        SETTINGS_CLASS,
+        L"JesterStep Settings",
+        WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        360,
+        220,
+        parent,
+        nullptr,
+        GetModuleHandleW(nullptr),
+        nullptr
+    );
+
+    if (g_settings_hwnd) {
+        ShowWindow(g_settings_hwnd, SW_SHOW);
+        UpdateWindow(g_settings_hwnd);
+    }
+}
+
 static void show_root_menu(HWND hwnd, int x, int y) {
     HMENU menu = CreatePopupMenu();
 
     AppendMenuW(menu, MF_STRING, 1001, L"Notepad");
     AppendMenuW(menu, MF_STRING, 1002, L"Explorer");
     AppendMenuW(menu, MF_SEPARATOR, 0, nullptr);
+    AppendMenuW(menu, MF_STRING, 1005, L"Settings...");
     AppendMenuW(menu, MF_STRING, 1003, L"Reload");
     AppendMenuW(menu, MF_SEPARATOR, 0, nullptr);
     AppendMenuW(menu, MF_STRING, 1004, L"Exit JesterStep");
@@ -131,12 +399,15 @@ static void show_root_menu(HWND hwnd, int x, int y) {
         case 1004:
             DestroyWindow(hwnd);
             break;
+        case 1005:
+            show_settings_window(hwnd);
+            break;
         default:
             break;
     }
 }
 
-static LRESULT CALLBACK wndproc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+static LRESULT CALLBACK panel_wndproc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
     switch (msg) {
         case WM_CREATE:
             SetTimer(hwnd, 1, 1000, nullptr);
@@ -184,9 +455,14 @@ static LRESULT CALLBACK wndproc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lpara
             SetTextColor(hdc, RGB(230, 230, 230));
 
             HFONT font = CreateFontW(
-                18, 0, 0, 0,
+                18,
+                0,
+                0,
+                0,
                 FW_NORMAL,
-                FALSE, FALSE, FALSE,
+                FALSE,
+                FALSE,
+                FALSE,
                 DEFAULT_CHARSET,
                 OUT_DEFAULT_PRECIS,
                 CLIP_DEFAULT_PRECIS,
@@ -237,14 +513,14 @@ static LRESULT CALLBACK wndproc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lpara
 
 int WINAPI wWinMain(HINSTANCE instance, HINSTANCE, PWSTR, int) {
     WNDCLASSW wc{};
-    wc.lpfnWndProc = wndproc;
+    wc.lpfnWndProc = panel_wndproc;
     wc.hInstance = instance;
     wc.lpszClassName = APP_CLASS;
     wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
 
     RegisterClassW(&wc);
 
-    HWND hwnd = CreateWindowExW(
+    g_panel_hwnd = CreateWindowExW(
         WS_EX_TOPMOST | WS_EX_TOOLWINDOW,
         APP_CLASS,
         L"JesterStep",
@@ -252,19 +528,19 @@ int WINAPI wWinMain(HINSTANCE instance, HINSTANCE, PWSTR, int) {
         0,
         0,
         GetSystemMetrics(SM_CXSCREEN),
-        PANEL_HEIGHT,
+        g_panel_height,
         nullptr,
         nullptr,
         instance,
         nullptr
     );
 
-    if (!hwnd) {
+    if (!g_panel_hwnd) {
         return 1;
     }
 
-    ShowWindow(hwnd, SW_SHOW);
-    UpdateWindow(hwnd);
+    ShowWindow(g_panel_hwnd, SW_SHOW);
+    UpdateWindow(g_panel_hwnd);
 
     MSG msg{};
     while (GetMessageW(&msg, nullptr, 0, 0)) {
